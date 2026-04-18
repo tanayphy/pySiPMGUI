ef334658c9f44ff694fcb633615828112aa16712
tanayphy <146993441+tanayphy@users.noreply.github.com>
2026-04-18 22:46:00 +0530

    Update TexTronix_OSC.py

--- modules/TexTronix_OSC.py
+++ modules/TexTronix_OSC.py
@@ -1,25 +1,19 @@
 '''
- MIT License
+ #############################################################################################################################
 
-Copyright (c) 2024 Tanay Dey
+Copyright (c) 2024 Tanay Dey Creative Commons Attribution-NonCommercial 4.0
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Title: PySiPMGUI: A Universal Python-Based Software for Photodetector I-V Quality Assurance: From Underground Dark Matter Searches to Astroparticle Cherenkov Cameras
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+Author: Dr. Tanay Dey
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+Co Authors: Suraj Shaw, Ritabrata Banerjee, Pratik Majumdar, Satyaki Bhattacharya
+
+https://doi.org/10.48550/arXiv.2603.24781
+
+https://github.com/tanayphy/pySiPMGUI.git
+
+###############################################################################################################################
 '''
 
 import tkinter as Tk
